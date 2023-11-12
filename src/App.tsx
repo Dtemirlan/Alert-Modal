b593eabc7d0c2afe5baed75529dfc710b6f1df40
Dzhaparov Temirlan <temirlandzaparov820@gmail.com>
2023-11-12 21:23:12 +0600

    Отредоктированы ошибки

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-// App.tsx
+
 import  { useState } from 'react';
+import Alert from './components/Alert.tsx';
 import Modal from './components/Modal.tsx';
 
 type ButtonType = 'primary' | 'success' | 'danger' | 'warning';
@@ -11,7 +12,12 @@ interface ButtonConfig {
 }
 
 const App = () => {
+    const [showAlert, setShowAlert] = useState(true);
     const [showModal, setShowModal] = useState(false);
+
+    const closeAlert = () => {
+        setShowAlert(false);
+    };
 
     const openModal = () => {
         setShowModal(true);
@@ -35,7 +41,19 @@ const App = () => {
     ];
 
     return (
-        <div className="container mt-5 d-flex justify-content-center align-items-center">
+        <div className="container mt-5">
+            {showAlert && (
+                <>
+                    <Alert type="warning" onDismiss={closeAlert}>
+                        This is a warning type alert
+                    </Alert>
+
+                    <Alert type="success">
+                        This is a success type alert
+                    </Alert>
+                </>
+            )}
+
             <button className="btn btn-primary" onClick={openModal}>Open Modal</button>
             <Modal
                 show={showModal}
