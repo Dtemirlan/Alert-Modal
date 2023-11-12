80aafc6d660b608dcf6856b5eae609503e68a904
Dzhaparov Temirlan <temirlandzaparov820@gmail.com>
2023-11-12 21:17:59 +0600

    Сделана возможность передавать массив конфигурации кнопок, добавлен обработчик нажаний который заупускает алерт

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import { useState } from 'react';
-import Alert from './components/Alert.tsx';
+// App.tsx
+import  { useState } from 'react';
 import Modal from './components/Modal.tsx';
+
+type ButtonType = 'primary' | 'success' | 'danger' | 'warning';
+
+interface ButtonConfig {
+    type: ButtonType;
+    label: string;
+    onClick: () => void;
+}
 
 const App = () => {
     const [showModal, setShowModal] = useState(false);
@@ -13,25 +21,27 @@ const App = () => {
         setShowModal(false);
     };
 
-    const closeAlert = () => {
-        console.log('Alert closed');
+    const continueHandler = () => {
+        alert('Continue clicked');
     };
 
+    const cancelHandler = () => {
+        alert('Cancel clicked');
+    };
+
+    const buttons: ButtonConfig[] = [
+        { type: 'primary', label: 'Continue', onClick: continueHandler },
+        { type: 'danger', label: 'Close', onClick: cancelHandler },
+    ];
+
     return (
-        <div className="container mt-5">
-            <Alert type="warning" onDismiss={closeAlert}>
-                This is a warning type alert
-            </Alert>
-
-            <Alert type="success">
-                This is a success type alert
-            </Alert>
-
+        <div className="container mt-5 d-flex justify-content-center align-items-center">
             <button className="btn btn-primary" onClick={openModal}>Open Modal</button>
             <Modal
                 show={showModal}
                 onClose={closeModal}
                 title="Some kinda modal title"
+                buttons={buttons}
             >
                 <p>This is modal content</p>
             </Modal>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,23 @@
-import React, { ReactNode } from 'react';
+// Modal.tsx
+import React, {  ReactNode } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface ButtonConfig {
+    type: 'primary' | 'success' | 'danger' | 'warning';
+    label: string;
+    onClick: () => void;
+}
 
 interface ModalProps {
     show: boolean;
     onClose: () => void;
     title: string;
     children: ReactNode;
+    buttons?: ButtonConfig[];
 }
 
-const Modal: React.FC<ModalProps> = ({ show, onClose, title, children }) => {
-    const modalClass = show ? 'modal fade show' : 'modal fade';
+const Modal: React.FC<ModalProps> = ({ show, onClose, title, children, buttons }) => {
+    const modalClass = `modal fade text-center ${show ? 'show' : ''}`;
     return (
         <div className={modalClass} tabIndex={-1} role="dialog" style={{ display: show ? 'block' : 'none' }}>
             <div className="modal-dialog" role="document">
@@ -23,6 +31,15 @@ const Modal: React.FC<ModalProps> = ({ show, onClose, title, children }) => {
                     <div className="modal-body">
                         {children}
                     </div>
+                    {buttons && buttons.length > 0 && (
+                        <div className="modal-footer">
+                            {buttons.map((button, index) => (
+                                <button key={index} type="button" className={`btn btn-${button.type}`} onClick={button.onClick}>
+                                    {button.label}
+                                </button>
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
